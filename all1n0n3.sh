52754e0b170867551ffd5c24b122355bdfb628ab
nu11secur1ty <venvaropt@gmail.com>
2021-03-02 00:54:33 +0200

    Update all1n0n3.sh

--- all1n0n3.sh
+++ all1n0n3.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/open-distro/unattended-installation/all-in-one-installation.sh && bash ~/all-in-one-installation.sh
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
   exit 0;
