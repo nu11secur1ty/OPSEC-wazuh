117b116af1e3fbf87aec0fefe75e1e49c38cd40d
nu11secur1ty <venvaropt@gmail.com>
2021-03-02 01:06:01 +0200

    Update pass_changer.sh

--- pass_changer.sh
+++ pass_changer.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
+# @nu11secur1ty
+# Docs
+# https://documentation.wazuh.com/4.0/user-manual/elasticsearch/elastic_tuning.html#change-users-password
 cd /usr/share/elasticsearch/plugins/opendistro_security/tools/
 ./securityadmin.sh -backup /home/ -nhnv -cacert /etc/elasticsearch/certs/root-ca.pem -cert /etc/elasticsearch/certs/admin.pem -key /etc/elasticsearch/certs/admin.key -icl -h localhost
