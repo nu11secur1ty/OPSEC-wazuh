be8f247d6ab2499c8f050c58324608be2484ecb7
nu11secur1ty <venvaropt@gmail.com>
2021-03-01 23:10:36 +0200

    Update sakai.sh

--- sakai.sh
+++ sakai.sh
@@ -8,7 +8,10 @@ export JAVA_HOME=/usr/ && apt install openjdk-11-jdk
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
 echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
 apt-get update -y
-
-
+apt-get install wazuh-manager -y
+systemctl daemon-reload
+systemctl enable wazuh-manager
+systemctl start wazuh-manager
+systemctl status wazuh-manager 
 
   exit 0;
