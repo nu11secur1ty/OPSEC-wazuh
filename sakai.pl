6c358e8505e020454b46fd18907d1253382b396e
nu11secur1ty <venvaropt@gmail.com>
2021-03-01 21:21:16 +0200

    Update sakai.pl

--- sakai.pl
+++ sakai.pl
@@ -5,13 +5,13 @@ use warnings;
 use diagnostics;
 
 # Packages
-my $pack_ = `apt update && apt upgrade -y`;
+my $pack_ = `apt-get update && apt-get upgrade -y`;
 
 # Up OS
-my $UP = `apt dist-upgrade -y`;
+my $UP = `apt-get dist-upgrade -y`;
 
 # Need to install
-my $need_to_install = `apt install apache2 vim git curl default-jre default-jdk gcc -y`;
+my $need_to_install = `apt-get install apache2 vim git curl default-jre default-jdk gcc -y`;
 
 # Deploy Siem
 my $action_= `curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/open-distro/unattended-installation/all-in-one-installation.sh && bash ~/all-in-one-installation.sh`;
