7ae5060c7828c830642713cb1a6a42e93e83958d
nu11secur1ty <venvaropt@gmail.com>
2021-03-01 21:17:58 +0200

    Update sakai.sh

--- sakai.sh
+++ sakai.sh
@@ -1,8 +1,21 @@
-#!/usr/bin/bash
+#!/usr/bin/perl
+# @nu11secur1ty 2021
+use strict;
+use warnings;
+use diagnostics;
+
 # Packages
-apt update && apt upgrade -y 
-apt dist-upgrade -y
-apt install apache2 vim git curl default-jre default-jdk gcc -y
-curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/open-distro/unattended-installation/all-in-one-installation.sh && bash ~/all-in-one-installation.sh
-telinit 6
+my $pack_ = `apt update && apt upgrade -y`;
+
+# Up OS
+my $UP = `apt dist-upgrade -y`;
+
+# Need to install
+my $need_to_install = `apt install apache2 vim git curl default-jre default-jdk gcc -y`;
+
+# Deploy Siem
+my $action_= `curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/open-distro/unattended-installation/all-in-one-installation.sh && bash ~/all-in-one-installation.sh`;
+
+# Reload
+my $reload = `telinit 6`;
   exit 0;
