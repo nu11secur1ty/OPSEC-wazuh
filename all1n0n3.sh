00cf0fcb6b7201b75e9b36342c99f60928071d65
nu11secur1ty <venvaropt@gmail.com>
2021-03-01 23:55:42 +0200

    Create all1n0n3.sh

---
+++ all1n0n3.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/open-distro/unattended-installation/all-in-one-installation.sh && bash ~/all-in-one-installation.sh
+  exit 0;
