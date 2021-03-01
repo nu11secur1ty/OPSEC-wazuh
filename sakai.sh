6625ff1f10f1bb76f7ccb1ec40af0c7d957f9d8e
nu11secur1ty <venvaropt@gmail.com>
2021-03-01 23:06:48 +0200

    Update sakai.sh

--- sakai.sh
+++ sakai.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/bash
 # @nu11secur1ty 2021
-
-# Packages and cleaning
-rm -rf /etc/apt/sources.list.d/*wazuh.list*
-dpkg --configure -a
-apt-get update && apt-get upgrade -y
-
-# Up OS
-apt-get dist-upgrade -y
-
-# Need to install
-apt-get install vim git curl default-jre default-jdk gcc -y
-
-# Deploy Siem
-curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/open-distro/unattended-installation/all-in-one-installation.sh && bash ~/all-in-one-installation.sh
-
-# Reload
-rm -rf ~/all-in-one-installation.sh
+apt install curl apt-transport-https unzip wget libcap2-bin software-properties-common lsb-release gnupg2 -y
+echo 'deb http://deb.debian.org/debian stretch-backports main' > /etc/apt/sources.list.d/backports.list
+add-apt-repository ppa:openjdk-r/ppa
 
 
   exit 0;
