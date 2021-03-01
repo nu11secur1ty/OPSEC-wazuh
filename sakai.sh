02e32ec437aa059cc512640982f573147abe2698
nu11secur1ty <venvaropt@gmail.com>
2021-03-01 22:07:02 +0200

    Update sakai.sh

--- sakai.sh
+++ sakai.sh
@@ -14,6 +14,8 @@ apt-get install apache2 vim git curl default-jre default-jdk gcc -y
 
 # Deploy Siem
 curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/open-distro/unattended-installation/all-in-one-installation.sh && bash ~/all-in-one-installation.sh
+
 # Reload
+rm -rf ~/all-in-one-installation.sh
 telinit 6
   exit 0;
