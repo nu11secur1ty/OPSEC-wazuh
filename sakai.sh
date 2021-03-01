147e884538bd938ff4545a4c59ed3b95e4d691c0
nu11secur1ty <venvaropt@gmail.com>
2021-03-01 23:08:39 +0200

    Update sakai.sh

--- sakai.sh
+++ sakai.sh
@@ -3,6 +3,12 @@
 apt install curl apt-transport-https unzip wget libcap2-bin software-properties-common lsb-release gnupg2 -y
 echo 'deb http://deb.debian.org/debian stretch-backports main' > /etc/apt/sources.list.d/backports.list
 add-apt-repository ppa:openjdk-r/ppa
+apt update -y
+export JAVA_HOME=/usr/ && apt install openjdk-11-jdk
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
+echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+apt-get update -y
+
 
 
   exit 0;
