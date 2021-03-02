c98e72dffa5204863f227622099ab2dd9abb55e3
nu11secur1ty <venvaropt@gmail.com>
2021-03-02 21:02:13 +0200

    Update sakaisamurai.sh

--- sakaisamurai.sh
+++ sakaisamurai.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 # @nu11secur1ty
-apt-get remove filebeat -y
-apt-get remove --purge filebeat -y
-apt-get remove --auto-remove opendistroforelasticsearch -y
-apt-get remove opendistroforelasticsearch-kibana -y
-apt-get remove --purge opendistroforelasticsearch-kibana -y
+apt-get purge filebeat -y
+apt-get purge --purge filebeat -y
+apt-get purge --auto-remove opendistroforelasticsearch -y
+apt-get purge opendistroforelasticsearch-kibana -y
+apt-get purge --purge opendistroforelasticsearch-kibana -y
 rm -rf /var/lib/elasticsearch
 rm -rf /etc/elasticsearch
 rm -rf /etc/apt/sources.list.d/*wazuh*
