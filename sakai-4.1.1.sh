2c5492b655bf6d0e5b057d4783e401f188997fb4
nu11secur1ty <venvaropt@gmail.com>
2021-03-03 15:03:34 +0200

    Update sakai-4.1.1.sh

--- sakai-4.1.1.sh
+++ sakai-4.1.1.sh
@@ -449,5 +449,5 @@ main() {
 }
 
 main "$@"
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 
+# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
