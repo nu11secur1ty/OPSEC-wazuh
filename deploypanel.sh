cb33919fcbc2860e967e011df28fc946fef6f7a9
nu11secur1ty <venvaropt@gmail.com>
2021-03-04 19:56:29 +0200

    Update deploypanel.sh

--- deploypanel.sh
+++ deploypanel.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 rm -rf /var/www/html/*
 cp -avr OPSEC-PANEL/* /var/www/html/
+  rm /etc/apache2/sites-enabled/000-default.conf
+  cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
   exit 0;
