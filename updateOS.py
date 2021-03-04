ac41d6e5263f4578e39a6c84208ce5406d29a16d
nu11secur1ty <venvaropt@gmail.com>
2021-03-04 19:46:01 +0200

    Create updateOS.py

---
+++ updateOS.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import os
+
+os.system("apt update -y")
+os.system("apt dist-upgrade -y")
+os.system("apt autoremove -y") 
+os.system("dpkg --configure -a")
+os.system("apt --fix-broken install -y")
