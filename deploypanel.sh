23d5498968afc00157c298b2d0a15ff07f2f1351
nu11secur1ty <venvaropt@gmail.com>
2021-03-04 22:42:33 +0200

    Update deploypanel.sh

--- deploypanel.sh
+++ deploypanel.sh
@@ -3,4 +3,6 @@ rm -rf /var/www/html/*
 cp -avr OPSEC-PANEL/* /var/www/html/
   rm /etc/apache2/sites-enabled/000-default.conf
   cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
+  echo "Generate a password for access..."
+  echo "htpasswd -c /sec/dir/.htpasswd your_user"
   exit 0;
