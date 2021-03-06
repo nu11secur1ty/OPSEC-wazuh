e8f42405e761ef21024a61a4407e7affed72b463
nu11secur1ty <venvaropt@gmail.com>
2021-03-06 12:32:07 +0200

    Update cleankey.sh

--- cleankey.sh
+++ cleankey.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-apt-key del --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 
-apt-key del --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+#apt-key del --keyserver keyserver.ubuntu.com --recv-keys 04EE7237B7D453EC 
+#apt-key del --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
 apt-get update && apt-get upgrade && apt-get autoremove -y
