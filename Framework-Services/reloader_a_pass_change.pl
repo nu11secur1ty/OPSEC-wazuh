d9ff2b08016624e388f54c257b941356e74295be
nu11secur1ty <venvaropt@gmail.com>
2021-03-06 09:36:41 +0200

    Update reloader_a_pass_change.pl

--- Framework-Services/reloader_a_pass_change.pl
+++ Framework-Services/reloader_a_pass_change.pl
@@ -34,8 +34,17 @@ my $elastic_stat = `systemctl status elasticsearch.service`;
 my $filebeat_stat = `systemctl status filebeat.service`;
 my $wazuh_stat = `systemctl status wazuh-manager.service`;
 my $kibana_stat = `systemctl status kibana.service`;
-print "The Status is:\n$elastic_stat\n$filebeat_stat\n$wazuh_stat\n $kibana_stat\n";
 
-exit 0;
+print "The Status of elastic is:\n";
+print "$elastic_stat\n";
+
+print "The Status of filebeat is:\n";
+print "$filebeat_stat\n";
+
+print "The Status of wazuh is:\n";
+print "$wazuh_stat\n";
+
+print "The Status of kibana is:\n";
+print "$kibana_stat\n";
 
   exit 0;
