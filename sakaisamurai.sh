15e4d843cccd913c160fbc9bedbad5af905f63e9
nu11secur1ty <venvaropt@gmail.com>
2021-03-06 17:45:27 +0200

    Update sakaisamurai.sh

--- sakaisamurai.sh
+++ sakaisamurai.sh
@@ -4,15 +4,19 @@ kill -9 $(pgrep -f elasticsearch)
 kill -9 $(pgrep -f filebeat)
 kill -9 $(pgrep -f wazuh)
 kill -9 $(pgrep -f kibana)
+
 # Removing
 apt-get purge wazuh-manager -y
   rm -rf /etc/*wazuh*
+  
 apt-get purge filebeat -y
   rm -rf /etc/*filebeat*
+
 apt-get purge opendistroforelasticsearch -y
 apt purge elasticsearch -y
   rm -rf /etc/*elastic*
   rm -rf /usr/share/elasticsearch/
+
 apt-get purge --auto-remove opendistroforelasticsearch -y
 apt-get purge --auto-remove elasticsearch-oss opendistroforelasticsearch -y
 rm -rf /usr/share/*elasticsearch*
@@ -21,11 +25,12 @@ rm -rf /etc/init.d/*elasticsearch*
 
 rm -rf /usr/share/*elasticsearch*
 apt-get purge opendistroforelasticsearch-kibana -y
-  rm -rf /etc/*kibana*
+rm -rf /etc/*kibana*
 rm -rf /etc/*wazuh*
 rm -rf /var/lib/elasticsearch
 rm -rf /etc/elasticsearch
 rm -rf /etc/apt/sources.list.d/*wazuh*
+
 # cleaning
 apt-get update -y
 apt-get dist-upgrade -y
