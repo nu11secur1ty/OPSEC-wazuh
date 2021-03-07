dd5c6c832ca4a0a711ee9932b83c4afb67861265
nu11secur1ty <venvaropt@gmail.com>
2021-03-07 11:40:57 +0200

    Update all-in-one-installation.sh

--- all-in-one-installation.sh
+++ all-in-one-installation.sh
@@ -8,6 +8,7 @@
 # License (version 2) as published by the FSF - Free Software
 # Foundation.
 # https://documentation.wazuh.com/current/installation-guide/more-installation-alternatives/elastic-stack/all-in-one-deployment/unattended-installation.html
+# https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/elastic-stack/unattended-installation/all-in-one-installation.sh
 
 ## Check if system is based on yum or apt-get or zypper
 char="."
