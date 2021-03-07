f0b2dc3f60b8400f55bf7e9d6215e4a782adb76e
nu11secur1ty <venvaropt@gmail.com>
2021-03-07 11:43:56 +0200

    Update update-all-in-one-installation.sh

--- update-all-in-one-installation.sh
+++ update-all-in-one-installation.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/elastic-stack/unattended-installation/all-in-one-installation.sh && bash ~/all-in-one-installation.sh
+curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/elastic-stack/unattended-installation/all-in-one-installation.sh
