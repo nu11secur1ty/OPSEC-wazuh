53d8597e658d3f74c7d905d98ee7122cabd68a5f
nu11secur1ty <venvaropt@gmail.com>
2021-03-12 09:11:10 +0200

    Update opesecwazuhUBlatest.sh

--- Upgrade/opesecwazuhUBlatest.sh
+++ Upgrade/opesecwazuhUBlatest.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
+# nu11secur1ty automation 2021 OPSEC Ltd
+# Upgrading the Wazuh manager
+
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
 echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
 apt-get update
   apt-get install wazuh-manager
+
+# Disabling the Wazuh repository
+  
