25d91f86e3ee17667677abf13fd3d6c97c07ab79
nu11secur1ty <venvaropt@gmail.com>
2021-03-12 09:08:13 +0200

    Create opesecwazuhUBlatest.sh

---
+++ Upgrade/opesecwazuhUBlatest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
+echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+apt-get update
+  apt-get install wazuh-manager
