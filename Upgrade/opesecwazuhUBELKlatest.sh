76a78e556800c9eaef27b9a0e47b86cadfa79653
nu11secur1ty <venvaropt@gmail.com>
2021-03-12 09:16:48 +0200

    Update opesecwazuhUBELKlatest.sh

--- Upgrade/opesecwazuhUBELKlatest.sh
+++ Upgrade/opesecwazuhUBELKlatest.sh
@@ -19,5 +19,19 @@ systemctl stop kibana
   curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
   echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
   apt-get update
+  sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/wazuh.list
+apt-get update
   
-  
+# Upgrading Elasticsearch
+
+curl -X PUT "https://127.0.0.1:9200/_cluster/settings"  -u <username>:<password> -k -H 'Content-Type: application/json' -d'
+{
+  "persistent": {
+    "cluster.routing.allocation.enable": "primaries"
+  }
+}
+'
+echo "Give the user name and password"
+read user
+read pass
+curl -X POST "https://127.0.0.1:9200/_flush/synced" -u $user:$pass -k
